8cb9b04fe9674cfd40c890f36065553f0c8ca1de
rashid121 <31141345+rashid121@users.noreply.github.com>
2018-11-15 20:56:30 +0100

    Add files via upload
    Directives

--- angular/js/controllers/MainController.js
+++ angular/js/controllers/MainController.js
@@ -1,43 +1,54 @@
 app.controller('MainController', ['$scope', function ($scope) {
-    $scope.title = 'This Month\'s Bestsellers';
-    $scope.promo = 'The most popular books this month.';
-    $scope.products = [{
-            likes: 0,
-            dislikes: 0,
-            name: 'The Book of Trees',
-            price: 19,
-            pubdate: new Date('2014', '03', '08'),
-            cover: 'img/shoe.jpg'
+    $scope.move = {
+        icon: 'img/prog.jpg',
+        title: 'MOVE',
+        developer: 'MOVE, Inc.',
+        price: 0.99
+    };
+
+    $scope.shutterbugg = {
+        icon: 'img/sherlock.jpg',
+        title: 'Shutterbugg',
+        developer: 'Chico Dusty',
+        price: 2.99
+    };
+
+    $scope.gameboard = {
+        icon: 'img/shoe.jpg',
+        title: 'Gameboard',
+        developer: 'Armando P.',
+        price: 1.99
+    };
+    $scope.forecast = {
+        icon: 'img/foggy.jpg',
+        title: 'Forecast',
+        developer: 'Forecast',
+        price: 1.99
+    };
+
+    $scope.apps = [{
+            icon: 'img/foggy.jpg',
+            title: 'MOVE',
+            developer: 'MOVE,Inc.',
+            price: 0.99
         },
         {
-            likes: 0,
-            dislikes: 0,
-            name: 'Program or be Programmed',
-            price: 8,
-            pubdate: new Date('2013', '08', '01'),
-            cover: 'img/shoe.jpg'
+            icon: 'img/prog.jpg',
+            title: 'Shutterbugg',
+            developer: 'Chico Dusty',
+            price: 2.99
         },
         {
-            likes: 0,
-            dislikes: 0,
-            name: 'Harry Potter & The Prisoner of Azkaban',
-            price: 11.99,
-            pubdate: new Date('1999', '07', '08'),
-            cover: 'img/shoe.jpg'
+            icon: 'img/sherlock.jpg',
+            title: 'Gameboard',
+            developer: 'Armando P.',
+            price: 1.99
         },
         {
-            likes: 0,
-            dislikes: 0,
-            name: 'Ready Player One',
-            price: 7.99,
-            pubdate: new Date('2011', '08', '16'),
-            cover: 'img/shoe.jpg'
+            icon: 'img/shoe.jpg',
+            title: 'Forecast',
+            developer: 'Forecast',
+            price: 1.99
         }
     ];
-    $scope.plusOne = function (index) {
-        $scope.products[index].likes += 1;
-    };
-    $scope.minusOne = function (index) {
-        $scope.products[index].dislikes += 1;
-    };
 }]);
